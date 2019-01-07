3a10cc24f952383fd48b3dd7239dfac60211b4b1
lyg <lany192@qq.com>
2019-01-07 11:37:45 +0800

    Initial commit

---
+++ src/test/java/com/github/lany192/pay/PaySpringBootStarterApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.lany192.pay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaySpringBootStarterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
