59f7e2eef5f2bfdf9ef1b93f74fe21af5463469a
lyg <lany192@qq.com>
2019-06-12 15:59:16 +0800

    update

--- src/main/java/com/github/lany192/pay/alipay/service/AlipayTradeService.java
+++ src/main/java/com/github/lany192/pay/alipay/service/AlipayTradeService.java
@@ -2,9 +2,9 @@ package com.github.lany192.pay.alipay.service;
 
 import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
-import com.alipay.api.domain.AlipayTradeAppPayModel;
-import com.alipay.api.request.AlipayTradeAppPayRequest;
-import com.alipay.api.response.AlipayTradeAppPayResponse;
+import com.alipay.api.domain.*;
+import com.alipay.api.request.*;
+import com.alipay.api.response.*;
 import com.github.lany192.pay.alipay.config.AlipayProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,17 +30,139 @@ public class AlipayTradeService {
     }
 
     /**
-     * APP支付
+     * 支付宝支付
      *
      * @param bizModel 业务参数
-     * @return 返回结果
-     * @throws AlipayApiException
      */
-    public AlipayTradeAppPayResponse tradeAppPay(AlipayTradeAppPayModel bizModel) throws AlipayApiException {
+    public AlipayTradeAppPayResponse tradePay(AlipayTradeAppPayModel bizModel) throws AlipayApiException {
         AlipayTradeAppPayRequest request = new AlipayTradeAppPayRequest();
-        request.setBizModel(bizModel);
+        request.setBizModel(bizModel); //设置业务参数
         request.setNotifyUrl(properties.getNotifyUrl());
         //这里和普通的接口调用不同，使用的是sdkExecute
         return alipayClient.sdkExecute(request);
+    }
+
+    /**
+     * 交易查询
+     *
+     * @param outTradeNo
+     * @return
+     */
+    public AlipayTradeQueryResponse queryByOutTradeNo(String outTradeNo) throws AlipayApiException {
+        AlipayTradeQueryModel request = new AlipayTradeQueryModel();
+        request.setOutTradeNo(outTradeNo);
+        return query(request);
+    }
+
+    /**
+     * 交易查询
+     *
+     * @param tradeNo
+     * @return
+     */
+    public AlipayTradeQueryResponse queryByTradeNo(String tradeNo) throws AlipayApiException {
+        AlipayTradeQueryModel request = new AlipayTradeQueryModel();
+        request.setTradeNo(tradeNo);
+        return query(request);
+    }
+
+    /**
+     * 转账查询
+     *
+     * @param outBizNo 商户转账唯一订单号
+     * @return
+     */
+    public AlipayFundTransOrderQueryResponse queryByOutBizNo(String outBizNo) throws AlipayApiException {
+        AlipayFundTransOrderQueryModel request = new AlipayFundTransOrderQueryModel();
+        request.setOutBizNo(outBizNo);
+        return query(request);
+    }
+
+    /**
+     * 转账查询
+     *
+     * @param orderId 支付宝转账单据号
+     * @return
+     */
+    public AlipayFundTransOrderQueryResponse queryByOrderId(String orderId) throws AlipayApiException {
+        AlipayFundTransOrderQueryModel request = new AlipayFundTransOrderQueryModel();
+        request.setOrderId(orderId);
+        return query(request);
+    }
+
+    /**
+     * 交易查询
+     *
+     * @param tradeQueryModel
+     * @return
+     */
+    private AlipayTradeQueryResponse query(AlipayTradeQueryModel tradeQueryModel) throws AlipayApiException {
+        AlipayTradeQueryRequest request = new AlipayTradeQueryRequest();
+        request.setBizModel(tradeQueryModel);
+        return alipayClient.execute(request);
+    }
+
+    /**
+     * 转账查询
+     */
+    private AlipayFundTransOrderQueryResponse query(AlipayFundTransOrderQueryModel alipayFundTransOrderQueryModel) throws AlipayApiException {
+        AlipayFundTransOrderQueryRequest request = new AlipayFundTransOrderQueryRequest();
+        request.setBizModel(alipayFundTransOrderQueryModel);
+        return alipayClient.execute(request);
+    }
+
+    public AlipayTradeCancelResponse cancelByOutTradeNo(String outTradeNo) throws AlipayApiException {
+        AlipayTradeCancelModel model = new AlipayTradeCancelModel();
+        model.setOutTradeNo(outTradeNo);
+        AlipayTradeCancelRequest request = new AlipayTradeCancelRequest();
+        request.setBizModel(model);
+        return alipayClient.execute(request);
+    }
+
+    public AlipayTradeCancelResponse cancelByTradeNo(String tradeNo) throws AlipayApiException {
+        AlipayTradeCancelModel model = new AlipayTradeCancelModel();
+        model.setTradeNo(tradeNo);
+        AlipayTradeCancelRequest request = new AlipayTradeCancelRequest();
+        request.setBizModel(model);
+        return alipayClient.execute(request);
+    }
+
+    public AlipayTradeRefundResponse refundByOutTradeNo(String outTradeNo, String refundAmount) throws AlipayApiException {
+        AlipayTradeRefundModel request = new AlipayTradeRefundModel();
+        request.setOutTradeNo(outTradeNo);
+        request.setRefundAmount(refundAmount);
+        return refund(request);
+    }
+
+    public AlipayTradeRefundResponse refundByTradeNo(String tradeNo, String refundAmount) throws AlipayApiException {
+        AlipayTradeRefundModel request = new AlipayTradeRefundModel();
+        request.setTradeNo(tradeNo);
+        request.setRefundAmount(refundAmount);
+        return refund(request);
+    }
+
+    public AlipayTradeRefundResponse refund(AlipayTradeRefundModel tradeRefund) throws AlipayApiException {
+        AlipayTradeRefundRequest request = new AlipayTradeRefundRequest();
+        request.setBizModel(tradeRefund);
+        return alipayClient.execute(request);
+    }
+
+    /**
+     * 下载账单
+     */
+    public AlipayDataDataserviceBillDownloadurlQueryResponse downloadBill(
+            AlipayDataDataserviceBillDownloadurlQueryModel billDownloadurlQuery) throws AlipayApiException {
+        AlipayDataDataserviceBillDownloadurlQueryRequest request = new AlipayDataDataserviceBillDownloadurlQueryRequest();
+        request.setBizModel(billDownloadurlQuery);
+        return alipayClient.execute(request);
+    }
+
+    /**
+     * 支付宝转账到其他账户
+     */
+    public AlipayFundTransToaccountTransferResponse transferToAccount(AlipayFundTransToaccountTransferModel transfer) throws AlipayApiException {
+        AlipayFundTransToaccountTransferRequest request = new AlipayFundTransToaccountTransferRequest();
+        request.setBizModel(transfer);
+        return alipayClient.execute(request);
     }
 }
